f539e8a077141a63d0a0ce78cef55c84bba62cef
WantChane <143467549+WantChane@users.noreply.github.com>
2026-05-15 09:36:51 +0800

    chore(debug): 提取插件名变量

--- debug.ps1
+++ debug.ps1
@@ -1,20 +1,23 @@
-dotnet publish Flow.Launcher.Plugin.Zoxide -c Debug -r win-x64 --no-self-contained
+$pluginName = "Zoxide"
+$projectName = "Flow.Launcher.Plugin.$pluginName"
+
+dotnet publish $projectName -c Debug -r win-x64 --no-self-contained
 
 $scoopPath = "$((Get-Item (Get-Command scoop.ps1).Path).Directory.Parent.FullName)"
 $flowLauncherExe = "$scoopPath\apps\Flow-Launcher\current\Flow.Launcher.exe"
 $flowLauncherPlugin = "$scoopPath\persist\Flow-Launcher\UserData\Plugins"
 
-
 if (Test-Path $flowLauncherExe) {
     Stop-Process -Name "Flow.Launcher" -Force -ErrorAction SilentlyContinue
     Start-Sleep -Seconds 2
 
-    if (Test-Path "$flowLauncherPlugin\Zoxide") {
-        Remove-Item -Recurse -Force "$flowLauncherPlugin\Zoxide"
+    $pluginDest = "$flowLauncherPlugin\$pluginName"
+    if (Test-Path $pluginDest) {
+        Remove-Item -Recurse -Force $pluginDest
     }
 
-    Copy-Item "Flow.Launcher.Plugin.Zoxide\bin\Debug\win-x64\publish" "$flowLauncherPlugin\" -Recurse -Force
-    Rename-Item -Path "$flowLauncherPlugin\publish" -NewName "Zoxide"
+    Copy-Item "$projectName\bin\Debug\win-x64\publish" "$flowLauncherPlugin\" -Recurse -Force
+    Rename-Item -Path "$flowLauncherPlugin\publish" -NewName $pluginName
 
     Start-Sleep -Seconds 2
     Start-Process $flowLauncherExe
